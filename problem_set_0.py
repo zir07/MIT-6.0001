dcfd62bdaf05b0bc3d0b31f522bb478803e0311b
Zain Imran <zaindy@gmail.com>
2025-04-30 16:00:37 +0500

    Initial commit with problem_set_0.py

---
+++ problem_set_0.py
@@ -0,0 +1,7 @@
+first_number=int(input("Enter the first number: "))
+second_number=int(input("Enter the second number: "))
+import math
+power=(first_number**second_number)
+print(f'{first_number} raised to {second_number} is {power}')
+logarithm=(math.log2(first_number))
+print(f'The base 2 log of {first_number} is {logarithm}')
